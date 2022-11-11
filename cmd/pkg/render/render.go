0a6219aa2e27ac879085c4c3b3cb9694365b9292
Pawan Sharma <pawansharma93520@gmail.com>
2022-11-11 23:33:05 +0530

    my initial commit

--- cmd/pkg/render/render.go
+++ cmd/pkg/render/render.go
@@ -24,27 +24,59 @@ var tc = make(map[string]*template.Template)
 
 }
 */
-func Render_Template(w http.ResponseWriter, tmpl string) {
-	_, available := tc[tmpl]
-	if !available {
-		fmt.Println("temp not available but we are now creating a new template to use")
-		parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl") // using .tmpl extension
-		// parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")  // // using .html extension
-		if err != nil {
-			log.Fatalln(err)
+func Render_Template(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	//  check to see if we already have the template in our cache
+	_, inMap := tc[t]
+	if !inMap {
+		/*
+			fmt.Println("temp not available but we are now creating a new template to use")
+			parsedTemplate, err := template.ParseFiles("./templates/"+t, "./templates/base.layout.tmpl") // using .tmpl extension
+			// parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")  // // using .html extension
+			if err != nil {
+				log.Fatalln(err)
+			}
+			err = parsedTemplate.Execute(w, nil)
+			if err != nil {
+				fmt.Println("error while parsing template : ", err)
+				// fmt.Fprintln(w, fmt.Sprintf("error while parsing template : %s\n", err))
+			}
+			tc[tmpl] = parsedTemplate
+
+		*/
+
+		// need to create the template
+		if err = createTemplateCache(t); err != nil {
+			return
 		}
-		err = parsedTemplate.Execute(w, nil)
-		if err != nil {
-			fmt.Println("error while parsing template : ", err)
-			// fmt.Fprintln(w, fmt.Sprintf("error while parsing template : %s\n", err))
-		}
-		tc[tmpl] = parsedTemplate
 	} else {
+
+		// we have the template in the cache
 		fmt.Println("using a cache template")
-		err := tc[tmpl].Execute(w, nil)
-		if err != nil {
-			fmt.Println("error while parsing template : ", err)
-			// fmt.Fprintln(w, fmt.Sprintf("error while parsing template : %s\n", err))
-		}
+		/*
+			err := tc[tmpl].Execute(w, nil)
+			if err != nil {
+				fmt.Println("error while parsing template : ", err)
+				// fmt.Fprintln(w, fmt.Sprintf("error while parsing template : %s\n", err))
+			}
+		*/
 	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./template/%s", t),
+		"./template/base.layout.tmpl",
+	}
+	parsedTemplate, err := template.ParseFiles(templates...)
+	if err != nil {
+		log.Println("failed to parse template files")
+	}
+	tc[t] = parsedTemplate
+	return nil
 }
